6239bd9c6dd071870a36616109c8e4cd0afd0f19
Kartik Thakkar <thakkar.kartik2011@gmail.com>
2020-03-18 16:35:00 +0530

    2nd Commit

--- index.php
+++ index.php
@@ -15,38 +15,36 @@
         <!-- Page Content -->
         <h1>User Details</h1>
         <hr>
-        <a href="AddNewUser.php" class=""> Add New </a>
-        <br/><br/>
-    <?php
+        <a href="AddNewUser.php" class="btn btn-primary"> Add New </a>
+        <br /><br />
+        <?php
         $sql = "SELECT * FROM tblUser";
-        if ($result = mysqli_query($link, $sql)) 
-        {
-           
+        if ($result = mysqli_query($link, $sql)) {
+
         ?>
-        <table class="table table-active table-bordered">
-            <thead>
-                <tr>
-                    <th> # </th>
-                    <th> Name </th>
-                    <th> Email</th>
-                    <th> Contact No </th>
-                </tr>
-            </thead>
-            <tbody>
-            <?php 
-            while($row = mysqli_fetch_array($result))
-            {
-                
-            ?>
-            <tr>
-                    <td> <?php echo($row['UserID']); ?> </td>
-                    <td> <?php echo($row['FirstName']); ?> </td>
-                    <td> <?php echo($row['Email']); ?> </td>
-                    <td> <?php echo($row['ContactNo']); ?> </td>
-                </tr>
-            <?php } ?>       
-            </tbody>
-        </table>
+            <table class="table table-active table-bordered">
+                <thead>
+                    <tr>
+                        <th> # </th>
+                        <th> Name </th>
+                        <th> Email</th>
+                        <th> Contact No </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    while ($row = mysqli_fetch_array($result)) {
+
+                    ?>
+                        <tr>
+                            <td> <?php echo ($row['UserID']); ?> </td>
+                            <td> <?php echo ($row['FirstName']); ?> </td>
+                            <td> <?php echo ($row['Email']); ?> </td>
+                            <td> <?php echo ($row['ContactNo']); ?> </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
         <?php
         }
         ?>
