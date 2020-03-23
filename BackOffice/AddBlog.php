66cf62baf5395f232cb656864f3545bdcd7be77f
Kartik Thakkar <thakkar.kartik2011@gmail.com>
2020-03-24 01:57:17 +0530

    Update AddBlog.php

--- BackOffice/AddBlog.php
+++ BackOffice/AddBlog.php
@@ -4,6 +4,9 @@
     <?php include('scripts.php'); ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script type="text/javascript" src="tinymce/tinymce.min.js"></script>
+    
+    </head>
+
 <script>
 	tinymce.init({
 	  selector: 'textarea#txtContent',  //Change this value according to your HTML
@@ -16,6 +19,7 @@
 </head>
 
 <body>
+    
     <?php include('header.php'); ?>
     <div id="content-wrapper">
 
@@ -36,11 +40,11 @@
             </h6>
             <div class="form-group">
                 <label for="txtTitle"> Title of Blog </label>
-                <input type="text" name='txtTitle' class="form-control" placeholder="Enter Blog Title"/>
+                <input type="text" name-='txtTitle' class="form-control" placeholder="Enter Blog Title" required/>
             </div>
             <div class="form-group">
                 <label for="txtTags"> Select Tags for Blog (comma seprated) </label>
-                <input type="text" name='txtTags' class="form-control" placeholder="Enter Tags"/>
+                <input type="text" name='txtTags' id="txtTags" class="form-control" placeholder="Enter Tags"/>
             </div>
             <textarea name="txtContent" id="txtContent" rows="50" class="form-control"></textarea>
            <br />
