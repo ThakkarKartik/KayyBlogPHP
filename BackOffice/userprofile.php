b4ce69080a6faa4b023519154e4648ae58fb0503
Kartik Thakkar <thakkar.kartik2011@gmail.com>
2020-03-28 19:44:48 +0530

    Admin

--- BackOffice/userprofile.php
+++ BackOffice/userprofile.php
@@ -3,6 +3,17 @@
 <head>
     <?php include('links.php'); ?>
     <title> User Profile </title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script type="text/javascript" src="tinymce/tinymce.min.js"></script>
+    <script>
+	tinymce.init({
+	  selector: 'textarea#txtContent',  //Change this value according to your HTML
+      auto_focus: 'element1',
+      plugins: "image media link table",
+
+	});
+</script>
+
 </head>
 
 <body>
@@ -22,37 +33,46 @@
                     <li class="breadcrumb-item">
                         <a href="Users.php"> Home </a>
                     </li>
-                    <li class="breadcrumb-item active"> Profile of <?php echo($row['FullName'])?></li>
+                    <li class="breadcrumb-item active"> Profile of <?php echo ($row['FullName']) ?></li>
                 </ol>
                 <!-- Page Content -->
-                <div class="row">
-                <div class="col-lg-3 col-sm-6">
-                    <?php 
-                        $ImageName = "NoImage.png";
-                        if ($row['ProfPic'] != null) {
-                            $ImageName = 'Users/' + $row["ProfPic"];
-                        }
-                    ?>
-                    <img src='../UploadedFiles/<?php echo($ImageName) ?>' class="img img-thumbnail"/>
-                </div>
-                <div class="col-lg-3 col-sm-6">
-                    <div class="form-group">
-                        <label for="txtName"> Full Name :</label>
-                        <input type="text" class="form-control" value="<?php echo($row["FullName"]) ?>">
+                <form method="POST" enctype="multipart/form-data">
+                    <div class="row">
+                        <div class="col-lg-3 col-sm-6">
+                            <?php
+                            $ImageName = "NoImage.png";
+                            if ($row['ProfPic'] != null) {
+                                $ImageName = 'Users/' + $row["ProfPic"];
+                            }
+                            ?>
+                            <img src='../UploadedFiles/<?php echo ($ImageName) ?>' id="imgUser" name="imgUser" class="img img-thumbnail" />
+                            <input type="file" name="UserPic" id="UserPic" style="visibility: hidden" />
+                        </div>
+                        <div class="col-lg-3 col-sm-6">
+                            <div class="form-group">
+                                <label for="txtName"> Full Name :</label>
+                                <input type="text" class="form-control" value="<?php echo ($row["FullName"]) ?>">
+                            </div>
+                            <div class="form-group">
+                                <label for="txtContactNo"> Contact No :</label>
+                                <input type="text" class="form-control" value="<?php echo ($row["ContactNo"]) ?>">
+                            </div>
+                            <div class="form-group">
+                                <label for="txtEmail"> E-Mail Address :</label>
+                                <input type="text" class="form-control" value="<?php echo ($row["Email"]) ?>">
+                            </div>
+                            
+                        </div>
+                        <div class="col-lg-6 col-sm-12">
+                        <textarea name="txtContent" id="txtContent" class="form-control"></textarea>
+                        </div>
+                        <div class="container">
+                            <br/>
+                            <input type="submit" class="btn btn-primary offset-5" value="Save Changes"/>
+                        </div>
                     </div>
-                    <div class="form-group">
-                    <label for="txtContactNo"> Contact No :</label>
-                        <input type="text" class="form-control" value="<?php echo($row["ContactNo"]) ?>">
-                    </div>
-                    <div class="form-group">
-                    <label for="txtEmail"> E-Mail Address :</label>
-                        <input type="text" class="form-control" value="<?php echo($row["Email"]) ?>">
-                    </div>
-                </div>
-                </div>
-
+                </form>
                 <!-- Page Content Ends Here -->
-
             </div>
         </div>
     <?php
@@ -61,5 +81,14 @@
     <?php include('footer.php'); ?>
 </body>
 <?php include('scripts.php'); ?>
+<script>
+    $('#imgUser').click(function() {
+        $('#UserPic').click();
+    });
+    $('#UserPic').change(function() {
+        //$('#UserPic').click();
+        alert("Image Chnaged");
+    });
+</script>
 
 </html>
